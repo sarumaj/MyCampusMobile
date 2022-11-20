04b22097f02ed903dac10901d282ae033ffff463
sarumaj <71898979+sarumaj@users.noreply.github.com>
2022-11-20 22:12:04 +0100

    Delete .buildozer/android/platform/build-arm64-v8a_armeabi-v7a directory

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/mycampus_mobile/obj/local/arm64-v8a/objs-debug/SDL2_image/IMG_WIC.o.d
+++
@@ -1,2 +0,0 @@
-/home/dawid/GitHub/MyCampusMobile/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/SDL2_image/IMG_WIC.o: \
-  /home/dawid/GitHub/MyCampusMobile/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/IMG_WIC.c
--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/mycampus_mobile/obj/local/arm64-v8a/objs-debug/hidapi/src/hidapi/android/hid.o.d
+++
@@ -1,5 +0,0 @@
-/home/dawid/GitHub/MyCampusMobile/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/hidapi/src/hidapi/android/hid.o: \
-  /home/dawid/GitHub/MyCampusMobile/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/hidapi/android/hid.cpp \
-  /home/dawid/GitHub/MyCampusMobile/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/hidapi/android/../hidapi/hidapi.h
-
-/home/dawid/GitHub/MyCampusMobile/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/hidapi/android/../hidapi/hidapi.h:
